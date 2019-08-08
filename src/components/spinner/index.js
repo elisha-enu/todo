df1cf6463ff240c7d06e6d023041b5d51ce73b4d
elisha-enu <elishaenu@gmail.com>
2019-08-08 21:46:35 +0700

    add spinner

---
+++ src/components/spinner/index.js
@@ -0,0 +1,30 @@
+import 'bootstrap/dist/css/bootstrap.css';
+import React from 'react'
+import PropTypes from 'prop-types'
+import Button from 'react-bootstrap/Button'
+import Modal from 'react-bootstrap/Modal'
+import Spinner from 'react-bootstrap/Spinner'
+
+const Spinners = ({
+  show,
+  onHide,
+}) => {
+  return (
+    <Modal show={show} onHide={onHide}>
+        <Spinner animation="border" role="status">
+            <span className="sr-only">Loading...</span>
+        </Spinner>
+    </Modal>
+  )
+}
+
+Spinners.propTypes = {
+  show: PropTypes.bool,
+  onHide: PropTypes.func,
+}
+
+Spinners.defaultProps = {
+  onHide: () => {},
+}
+
+export default Spinners
